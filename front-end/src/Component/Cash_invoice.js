546e776070b1c17b9d1fbf87ba33064e9e61a5fa
Zeera <zeeraratcha@gmail.com>
2022-06-24 16:13:53 +0700

    cant run toFixed function

--- front-end/src/Component/Cash_invoice.js
+++ front-end/src/Component/Cash_invoice.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Component/Cash_invoice.css';
 import { SiVerizon } from "react-icons/si";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
 
 export default function Cash_invoice() {
+    
+    //----------------- 1 -----------------//
+    const [quantity1, setQuantity1] = useState();
+    const [price1, setPrice1] = useState();
+    const [amount1, setAmount1] = useState();
 
+    //----------------- 2 -----------------//
+    const [quantity2, setQuantity2] = useState();
+    const [price2, setPrice2] = useState();
+    const [amount2, setAmount2] = useState();
 
+    //----------------- 3 -----------------//
+    const [quantity3, setQuantity3] = useState();
+    const [price3, setPrice3] = useState();
+    const [amount3, setAmount3] = useState();
+
+    //----------------- 4 -----------------//
+    const [quantity4, setQuantity4] = useState();
+    const [price4, setPrice4] = useState();
+    const [amount4, setAmount4] = useState();
+
+    //----------------- Sum -----------------//
+    const [sum, setSum] = useState();
+
+    //----------------- Discount -----------------//
+    const [numberdiscount, setNumberdiscount] = useState();
+    const [displayDiscount, setDisplayDiscount] = useState();
+    const [discount, setDiscount] = useState();
+
+    //----------------- Tax -----------------//
+    const [tax, setTax] = useState();
+
+    //----------------- Net Total -----------------//
+    const [netTotal, setNetTotal] = useState();
+
+    //----------------- useEffect -----------------//
+
+    useEffect(() => {
+        //1
+        setAmount1(parseFloat(quantity1) * parseFloat(price1)) //amount1
+        //2
+        setAmount2(parseFloat(quantity2) * parseFloat(price2)) //amount2
+        //3
+        setAmount3(parseFloat(quantity3) * parseFloat(price3)) //amount3
+        //4
+        setAmount4(parseFloat(quantity4) * parseFloat(price4)) //amount4
+        //Sum
+        setSum(parseFloat(amount1) + parseFloat(amount2) + parseFloat(amount3) + parseFloat(amount4)) //Sum
+        //Discount
+        setDiscount(parseFloat(sum) - parseFloat(sum) * parseFloat(numberdiscount/100)) //Discount
+        //Display Discount
+        setDisplayDiscount(parseFloat(sum) * parseFloat(numberdiscount/100)) //Display Discount
+        //Tax
+        setTax(parseFloat(sum) - parseFloat(sum) * parseFloat(numberdiscount/100) *7/100) //Tax
+        //Net Total
+        setNetTotal(parseFloat(sum) - parseFloat(sum) * parseFloat(numberdiscount/100) + parseFloat(sum) - parseFloat(sum) * parseFloat(numberdiscount/100))//Discount
+
+    }, [price1, price2, price3, price4, amount1, amount2, amount3, amount4, quantity1, quantity2, quantity3, quantity4, sum, numberdiscount, discount, tax, netTotal])
+    
 
   return (<>
 
@@ -48,16 +106,16 @@ export default function Cash_invoice() {
                         <div className='my_company' style={{fontSize: '13px',textAlign: 'left'}}>
 
                             <div>
-                                <p>ชื่อบริษัท: <input type="text" />
-                                    <span><br/>ชื่อ:  <input type="text" /></span>
-                                    <span><br/>ที่อยู่: <input type="text" /></span>
-                                    <span><br/>เลขประจำตัวผู้เสียภาษี: <input type="text" /></span>
+                                <p>ชื่อบริษัท: <input type="text" placeholder="ตัวอย่าง บริษัท ขายดี จำกัด" />
+                                    <span><br/>ชื่อ:  <input type="text"placeholder="ตัวอย่าง นาย เฮง ทำมาค้าขึ้น"/></span>
+                                    <span><br/>ที่อยู่: <input type="text" placeholder="ตัวอย่าง 234 ซอย สุขุมวิท 21 แขวงคลองเตยเหนือ เขตวัฒนา กรุงเทพมหานคร 10110 "/></span>
+                                    <span><br/>เลขประจำตัวผู้เสียภาษี: <input type="text" placeholder="0000000000000"/></span>
                                 </p>
                             </div>
                             
                             <div>
                                 <p>วันที่: <input type="date" id="dt"/>
-                                    <span><br/>ผู้ขาย: <input type="text" /></span>
+                                    <span><br/>ผู้ขาย: <input type="text" placeholder="เฮง เฮง"/></span>
                                     <span><br/>ครบกำหนด: <input type="date" id="dt"/></span>
                                 </p>
                             </div>
@@ -66,10 +124,10 @@ export default function Cash_invoice() {
                         
 
                         <div className='customer_company' style={{fontSize: '13px', textAlign: 'left'}}>
-                            <p>ชื่อบริษัทลูกค้า: <input type="text" />
-                                <span><br/>ชื่อลูกค้า: <input type="text" /></span>
-                                <span><br/>ที่อยู่ลูกค้า: <input type="text" /></span>
-                                <span><br/>เลขประจำตัวผู้เสียภาษี: <input type="text" /></span>
+                            <p>ชื่อบริษัทลูกค้า: <input type="text" placeholder="ตัวอย่าง บริษัท ชอบซื้อ จำกัด"/>
+                                <span><br/>ชื่อลูกค้า: <input type="text" placeholder="ตัวอย่าง นาย รักดี รักมั่น"/></span>
+                                <span><br/>ที่อยู่ลูกค้า: <input type="text" placeholder="ตัวอย่าง 123 ถนนสุรวงศ์ แขวงสุริยวงศ เขตบางรัก กรุงเทพ 10500"/></span>
+                                <span><br/>เลขประจำตัวผู้เสียภาษี: <input type="text" placeholder="1234567891234"/></span>
                             </p>
                         </div>
 
@@ -90,39 +148,39 @@ export default function Cash_invoice() {
                                 <tbody>
 
                                     <tr>
-                                        <td><input style={{textAlign: "left"}} type="text" placeholder="1"/></td>
+                                        <td><input style={{textAlign: "left"}} type="float" placeholder="1"/></td>
                                         <td><input style={{textAlign: "left"}} type="text" placeholder="ชื่อสินค้า"/></td>
-                                        <td><input type="text" placeholder="1"/></td>
+                                        <td><input type="float" placeholder="1" value={quantity1} onChange={(e) => setQuantity1(e.target.value)}/></td>
                                         <td><input type="text" placeholder="ชิ้น"/></td>
-                                        <td><input type="text" placeholder="100.00"/></td>
-                                        <td><input type="text" placeholder="100.00"/></td>
+                                        <td><input type="float" placeholder="100.00" value={price1} onChange={(e) => setPrice1(e.target.value)}/></td>
+                                        <td><p>{amount1}</p></td>
                                     </tr>
 
                                     <tr>
-                                        <td><input style={{textAlign: "left"}} type="text" placeholder="2"/></td>
-                                        <td><input style={{textAlign: "left"}}type="text" placeholder="ชื่อสินค้า"/></td>
-                                        <td><input type="text" placeholder="1"/></td>
-                                        <td><input type="text" placeholder="ตัว"/></td>
-                                        <td><input type="text" placeholder="0.00"/></td>
-                                        <td><input type="text" placeholder="0.00"/></td>
-                                    </tr>
-
-                                    <tr>
-                                        <td><input style={{textAlign: "left"}} type="text" placeholder="3"/></td>
+                                        <td><input style={{textAlign: "left"}} type="float" placeholder="1"/></td>
                                         <td><input style={{textAlign: "left"}} type="text" placeholder="ชื่อสินค้า"/></td>
-                                        <td><input type="text" placeholder="1"/></td>
-                                        <td><input type="text" placeholder="ตัว"/></td>
-                                        <td><input type="text" placeholder="0.00"/></td>
-                                        <td><input type="text" placeholder="0.00"/></td>
+                                        <td><input type="float" placeholder="1" value={quantity2} onChange={(e) => setQuantity2(e.target.value)}/></td>
+                                        <td><input type="text" placeholder="ชิ้น"/></td>
+                                        <td><input type="float" placeholder="100.00" value={price2} onChange={(e) => setPrice2(e.target.value)}/></td>
+                                        <td><p>{amount2}</p></td>
                                     </tr>
 
                                     <tr>
-                                        <td><input style={{textAlign: "left"}} type="text" placeholder="4"/></td>
+                                        <td><input style={{textAlign: "left"}} type="float" placeholder="1"/></td>
                                         <td><input style={{textAlign: "left"}} type="text" placeholder="ชื่อสินค้า"/></td>
-                                        <td><input type="text" placeholder="1"/></td>
-                                        <td><input type="text" placeholder="ตัว"/></td>
-                                        <td><input type="text" placeholder="0.00"/></td>
-                                        <td><input type="text" placeholder="0.00"/></td>
+                                        <td><input type="float" placeholder="1" value={quantity3} onChange={(e) => setQuantity3(e.target.value)}/></td>
+                                        <td><input type="text" placeholder="ชิ้น"/></td>
+                                        <td><input type="float" placeholder="100.00" value={price3} onChange={(e) => setPrice3(e.target.value)}/></td>
+                                        <td><p>{amount3}</p></td>
+                                    </tr>
+
+                                    <tr>
+                                        <td><input style={{textAlign: "left"}} type="float" placeholder="1"/></td>
+                                        <td><input style={{textAlign: "left"}} type="text" placeholder="ชื่อสินค้า"/></td>
+                                        <td><input type="float" placeholder="1" value={quantity4} onChange={(e) => setQuantity4(e.target.value)}/></td>
+                                        <td><input type="text" placeholder="ชิ้น"/></td>
+                                        <td><input type="float" placeholder="100.00" value={price4} onChange={(e) => setPrice4(e.target.value)}/></td>
+                                        <td><p>{amount4}</p></td>
                                     </tr>
 
                                     <tr>
@@ -134,18 +192,19 @@ export default function Cash_invoice() {
 
 
                         <div className='summary' style={{fontSize: '13px'}}>
-                            <p>รวมเป็นเงิน:  <span className='result'><td><input type="text" placeholder="100.00"/></td></span>
-                                <span><br/>ส่วนลด <input style={{width: '5%'}} type="text" placeholder="5"/>%</span>         
-                                    <span className='result1'><td><input type="text" placeholder="5.00"/></td></span>
+                            <p>รวมเป็นเงิน:  <span className='result'><td><input type="float" placeholder="100.00" value={sum}/></td></span>
+                                <span><br/>ส่วนลด <input style={{width: '5%'}} type="float" placeholder="5" value={numberdiscount} onChange={(e) => setNumberdiscount(e.target.value)}/>%</span>         
+                                    <span className='result1'><td><input type="text" value={displayDiscount}/></td></span>
                                 
                                 <span><br/>ราคาหลังหักส่วนลด</span>   
-                                    <span className='result2'><td><input type="text" placeholder="95.00"/></td></span>
+                                    <span className='result2'><td><input type="float" placeholder="95.00" value={discount}/></td></span>
                                 
                                 <span><br/>ภาษีมูลค่าเพิ่ม 7%</span>    
-                                    <span className='result3'><td><input type="text" placeholder="6.65"/></td></span>
+                                    {/* <span className='result3'><td><input type="float" placeholder="6.65" value={tax}/></td></span> */}
+                                    <span className='result3'><td><p placeholder="6.65">{tax.toFixed(2)}</p></td></span>
                                 
                                 <span><br/>จำนวนเงินรวมทั้งสิ้น</span>   
-                                    <span className='result4'><td><input type="text" placeholder="101.65"/></td></span>
+                                    <span className='result4'><td><input type="float" placeholder="101.65" value={netTotal}/></td></span>
                             </p>
                         </div>
                         
