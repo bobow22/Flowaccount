b420956041ebf0bad7cdf5a7350b89c689f77b13
bobow22 <bobow.kj@gmail.com>
2022-06-29 16:52:20 +0700

    delect error line

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import Register from "./Login & Register/Register";
 import Login from "./Login & Register/Login";
 import Dashboard from "./Component/Dashboard";
 import Cash_invoice from "./Component/Cash_invoice";
-import From from "./Component/validation_Test";
+
 import axios from "axios";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
