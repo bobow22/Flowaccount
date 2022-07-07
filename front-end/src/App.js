2219270a8a4fe57f8413b637471bcaa97efa8dee
Zeera <zeeraratcha@gmail.com>
2022-07-07 17:30:05 +0700

    clone cash invoice

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import Register from "./Login & Register/Register";
 import Login from "./Login & Register/Login";
 import Dashboard from "./Component/Dashboard";
+import Dashboard_cash_invoice from "./Component/Dashboard_cash_invoice";
 import Cash_invoice from "./Component/Cash_invoice";
 import axios from "axios";
 import 'boxicons'
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//zeera
+
 function App() {
 
   useEffect(() => {
@@ -24,6 +25,7 @@ function App() {
       <Routes>
         <Route exact path="/CashInvoice" element={<Cash_invoice />} />
         <Route exact path="/Dashboard" element={<Dashboard />} />
+        <Route exact path="/DashboardCashInvoice" element={<Dashboard_cash_invoice />} />
         <Route exact path="/Register" element={<Register />} />
         <Route exact path="/" element={<Login />} />
       </Routes>
